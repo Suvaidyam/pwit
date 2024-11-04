3cb21d105c91dcdd802ca50a1b183de703145c8c
rsuvaidyam <rkrahul00011@gmail.com>
2024-11-04 06:16:55 +0000

    add

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="openDialog">Register</button>
+    <button class="w-[74px] h-10 text-h5" @click="openDialog">Register</button>
     <TransitionRoot as="template" :show="open">
         <Dialog class="relative z-20" @close="open = false">
             <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
@@ -15,38 +15,41 @@
                         leave-from="opacity-100 translate-y-0 sm:scale-100"
                         leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                         <DialogPanel
-                            class="relative transform overflow-hidden  bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-xl">
+                            class="relative transform overflow-hidden  bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-2xl">
                             <div class="bg-white px-4 w-full pb-4 pt-5 sm:p-6 sm:pb-4">
                                 <div class="mt-3 text-center">
-                                    <DialogTitle as="h3" class="text-xl font-semibold text-gray-900 text-center">Sign Up
+                                    <DialogTitle as="h3" class="text-h3 font-bold text-[#21272A] text-center">Sign Up
                                     </DialogTitle>
-                                    <p class="text-sm pt-2 text-center text-gray-500">Please provide the following
+                                    <p class="text-h5 pt-2 text-center text-trbase">Please provide the following
                                         information to continue </p>
                                 </div>
                                 <div class="flex flex-col gap-3 pt-4">
                                     <div class="flex flex-col gap-2 w-full">
                                         <label for="full_name" class="text-sm text-tatary">Full Name</label>
                                         <input v-model="full_name" type="text" id="full_name"
-                                            class="outline-none w-full border-b-2 bg-gray-50 px-3 py-2 placeholder:text-sm"
+                                            class="outline-none w-full border-b-2 bg-gray-50 px-3 h-12 text-h5"
                                             placeholder="Enter Full Name">
                                     </div>
                                     <div class="flex flex-col gap-2 w-full">
                                         <label for="" class="text-sm text-tatary">Email Address</label>
                                         <input v-model="email" type="email"
-                                            class="outline-none w-full border-b-2 bg-gray-50 px-3 py-2 placeholder:text-sm"
+                                            class="outline-none w-full border-b-2 bg-gray-50 px-3 h-12 text-h5"
                                             placeholder="Enter Email Address">
                                     </div>
                                     <div class="flex flex-col gap-2 w-full relative">
                                         <label for="" class="text-sm text-tatary">Password</label>
                                         <input v-model="password" :type="show_pass ? 'text' : 'password'"
-                                            class="outline-none w-full border-b-2 bg-gray-50 px-3 py-2 placeholder:text-sm"
+                                            class="outline-none w-full border-b-2 bg-gray-50 px-3 h-12 text-h5"
                                             placeholder="******">
                                         <span class="absolute right-2 top-10 text-xs cursor-pointer"
-                                            @click="show_pass = !show_pass">{{ show_pass ? 'hide' : 'show' }} </span>
+                                            @click="show_pass = !show_pass">
+                                            <EyeOff class="w-5 h-5 text-gray-500" v-if="show_pass"/>
+                                            <Eye class="w-5 h-5 text-gray-500" v-else/>
+                                        </span>
                                     </div>
                                     <div class="w-full border-b h-14">
                                         <button type="button"
-                                            class=" w-full justify-center rounded-md bg-secondary px-3 py-2.5 text-sm font-semibold text-white shadow-sm"
+                                            class=" w-full justify-center rounded-md bg-secondary px-3 h-12 text-h5 font-normal text-white shadow-sm"
                                             @click="register">Sign Up</button>
                                     </div>
                                     <div class="flex items-center justify-center">
@@ -67,9 +70,9 @@
 <script setup>
 import { ref ,watch,inject} from 'vue'
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline'
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
+import {Eye,EyeOff} from 'lucide-vue-next'
 
 const open = ref(false)
 const show_pass = ref(false)
