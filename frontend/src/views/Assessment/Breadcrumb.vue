1ae618e2db5d6cbcfe99542ab5712ef440d2d3fd
rsuvaidyam <rkrahul00011@gmail.com>
2024-11-15 06:36:36 +0000

    add btn

--- frontend/src/views/Assessment/Breadcrumb.vue
+++ frontend/src/views/Assessment/Breadcrumb.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="flex gap-2">
-        <svg @click="store.sidebar=true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="size-6 cursor-pointer block md:hidden">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-        </svg>
+        <Text  @click="store.sidebar=true" class="w-6 min-w-6 cursor-pointer block md:hidden"/>
         <p class="text-gray-800 text-sm">
             <router-link to="/">Home</router-link>
            / <router-link to="/funder-diagnostic">Funder Diagnostic</router-link> / <router-link to="/recommended">Recommended Principles</router-link> <span class="text-gray-400 truncate">/ {{ title }}</span>
@@ -14,6 +11,7 @@
 <script setup>
 import { ref ,inject, watch} from 'vue'
 import { useRoute } from 'vue-router';
+import {Text} from 'lucide-vue-next'
 
 const route = useRoute() 
 const store = inject('store')
--- frontend/src/views/Assessment/Results.vue
+++ frontend/src/views/Assessment/Results.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="w-full h-screen p-4">
         <div class="flex gap-2">
-            <svg @click="store.sidebar = true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer block md:hidden">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-            </svg>
+            <Text  @click="store.sidebar=true" class="w-6 min-w-6 cursor-pointer block md:hidden"/>
             <p class="text-gray-800 text-sm">
                 <router-link to="/">Home</router-link>
                 / <router-link to="/funder-diagnostic">Funder Diagnostic</router-link> / <router-link
@@ -13,13 +10,18 @@
                 <span class="text-gray-400 truncate"> / Results and Recommendtions</span>
             </p>
         </div>
-        <h1 class="text-h2 text-primary">Results and Recommendtions</h1>
+        <div class="flex justify-between items-center">
+            <h1 class="text-h3 md:text-h2 text-primary">Results and Recommendtions</h1>
+            <DownloadResults />
+        </div>
     </div>
 </template>
 
 <script setup>
 import { ref, watch, inject } from 'vue'
 import { useRoute } from 'vue-router';
+import DownloadResults from './DownloadResults.vue';
+import { Text } from 'lucide-vue-next'
 
 const route = useRoute()
 const title = ref(splitAtSecondCapital(route.path));
