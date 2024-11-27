89d8ce293c747c66fcaaa912984ef0c407549831
rsuvaidyam <rkrahul00011@gmail.com>
2024-11-27 06:40:38 +0000

    multiselect handle

--- pwit/controllers/assessment.py
+++ pwit/controllers/assessment.py
@@ -48,36 +48,62 @@ class AssessmentAPIs:
         if len(assessments) > 0:
             assessment = assessments[0]
             final_fields = {}
-            sf = None  # Keep track of the current Section Break
+            sf = None   
 
-            # Organize fields by section
             for field in meta.fields:
                 if field.fieldtype == "Section Break":
                     sf = field.fieldname
-                    final_fields[sf] = []  # Create a new entry for the section
-                elif field.fieldtype == "Link" and field.options == "Field Options":
+                    final_fields[sf] = []  
+                elif field.fieldtype in ["Link", "Table MultiSelect"] and field.options in ["Field Options", "Options Child"]:
                     if sf:
-                        final_fields[sf].append(field.fieldname)  # Add to the current section
+                        final_fields[sf].append(field)  # Add field to the section if sf is set
             data = {}
             total = 0
             # Calculate scores
             for section, fields in final_fields.items():
                 section_field = next((field for field in meta.fields if field.fieldname == section), None)
                 data[section_field.label] = 0
-                options = frappe.db.get_list(
-                    'Field Options',
-                    filters={
-                        'ref_doctype': doctype,
-                        'field': ["IN", fields]
-                    },
-                    fields=['name', 'field', 'score'],
-                    ignore_permissions=True
-                )
-                for option in options:
-                    # Check if the assessment matches this option
-                    if assessment.get(option.field) == option.name:
-                        data[section_field.label] += option.score
-                        total += option.score
+                table_fields = list(filter(lambda field: field.get('fieldtype') == 'Table MultiSelect', fields))
+                link_fields = list(filter(lambda field: field.get('fieldtype') == 'Link', fields))
+                if len(link_fields):
+                    options = frappe.db.get_list(
+                        'Field Options',
+                        filters={
+                            'ref_doctype': doctype,
+                            'field': ["IN", [field.fieldname for field in link_fields]]
+                        },
+                        fields=['name', 'field', 'score'],
+                        ignore_permissions=True
+                    )
+                    for option in options:
+                        # Check if the assessment matches this option
+                        if assessment.get(option.field) == option.name:
+                            data[section_field.label] += option.score
+                            total += option.score
+                if len(table_fields):
+                    for field in table_fields:
+                        field_options = frappe.db.get_list(
+                            'Options Child',
+                            filters={
+                                'parent': assessment.name,
+                                'parentfield': field.get('fieldname')
+                            },
+                            pluck='field_options',
+                            ignore_permissions=True
+                        )
+                        print("field_options",field_options)
+                        options = frappe.db.get_list(
+                            'Field Options',
+                            filters={
+                                'ref_doctype': doctype,
+                                'name': ["IN", field_options]
+                            },
+                            fields=['name', 'field', 'score'],
+                            ignore_permissions=True
+                        )
+                        for option in options:
+                            data[section_field.label] += option.score
+                            total += option.score
             average = round(total / len(final_fields),1)
             return {'code': 200,'data':{"average":average,"result":data}}
         else:
