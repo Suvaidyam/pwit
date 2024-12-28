f5cba804c0944b8145a3d044082ca08bb12fe951
rsuvaidyam <rkrahul00011@gmail.com>
2024-12-28 09:57:36 +0000

    add padding in home

--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -16,7 +16,7 @@
 		<div class="w-full bg-tatary px-4 md:px-8 lg:px-20 py-6 mt-7">
 			<!-- <h4 class="text-center text-h2 font-bold text-[#21272A] mt-6 font-primary tracking-wide leading-9">About the
 				tool</h4> -->
-			<p class="text-secondary text-h3 py-2 font-semibold">How to Use the Pay-What-It-Takes (PWIT) India Assistive Funder Toolkit</p>
+			<p class="text-secondary text-h4 md:text-h3 py-2 font-semibold">How to Use the Pay-What-It-Takes (PWIT) India Assistive Funder Toolkit</p>
 			<div class="flex font-normal text-sebase text-h5 tracking-wide leading-5">
 				<p class="text-justify">Welcome to the <a target="_blank"
 					href="https://www.bridgespan.org/our-global-reach/pay-what-it-takes-india-initiative"
@@ -110,7 +110,7 @@
 		</div>
 		<!--  -->
 		<div class=" w-full px-4 md:px-8 lg:px-20 py-6 ">
-			<p class="text-h2 text-primary font-semibold">About the Assistive Funder Toolkit</p>
+			<p class="text-h3 md:text-h2 text-primary font-semibold">About the Assistive Funder Toolkit</p>
 			<div class="flex h-full items-center flex-col md:flex-row mt-1">
 				<div class="w-full flex flex-col gap-3">
 					<h1 class=" text-h4 font-bold text-sebase leading-9 tracking-wide font-primary">What does the
@@ -263,7 +263,7 @@
 		<div class="w-full flex justify-center py-6 px-4">
 			<PickAssessment />
 		</div>
-		<div class="bg-secondary h-auto pb-52  relative px-4 md:px-8 lg:px-16">
+		<div class="bg-secondary h-auto pb-36 md:pb-40  relative px-4 md:px-8 lg:px-16">
 			<!-- SVG Background -->
 			<div class="absolute inset-0 flex justify-center gap-12 py-10">
 				<div class="relative">
@@ -302,7 +302,7 @@
 		</div>
 
 		<div
-			class="flex flex-col sm:flex-row h-[500px]  items-center justify-center gap-4 px-4 pb-8 relative bg-gray-100 ">
+			class="flex flex-col sm:flex-row h-[440px] md:h-[500px]  items-center justify-center gap-4 px-4 pb-8 relative bg-gray-100 ">
 			<ContactUs />
 		</div>
 		<FooterNav />
